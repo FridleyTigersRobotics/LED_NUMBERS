f2a415b8e04901676735566770b117f8ae6fb9cb
Terrrm <9992188+Terrrm@users.noreply.github.com>
2024-04-04 10:41:41 -0500

    LED numbers intial commit.

---
+++ LED_NUMBERS.ino
@@ -0,0 +1,305 @@
+#include <FastLED.h>    // if not found, get it here http://fastled.io/
+
+
+#define NUM_LEDS 64
+#define NUM_LEDS_IN_2 ( 18 )
+#define NUM_LEDS_IN_7 ( 10 )
+
+#define LED_CONTROL_PIN 3
+#define PATTERN_CHANGE_PIN 3
+
+CRGB leds[NUM_LEDS];
+
+// Pattern Functions
+void StripeHorizontal(void);
+void DotChase(void);
+void Meter(void);
+void Chant(void);
+void RainbowFull(void);
+
+
+void ( *PatternFunctions[] )( void ) = {
+  StripeHorizontal,
+  DotChase,
+  Meter,
+  Chant,
+  RainbowFull
+};
+uint8_t numPatterns = sizeof( PatternFunctions ) / sizeof( PatternFunctions[0] );
+
+CRGB GoldColor = CRGB(0x00, 0xFF, 0xDF);
+CRGB ColorA = CRGB::White;
+CRGB ColorB = GoldColor;
+
+uint8_t  patternId = 0;
+uint16_t buttonCount = 0;
+
+
+uint32_t patternHoldCount = 0;
+
+
+// This function sets up the ledsand tells the controller about them
+void setup() {
+    // sanity check delay - allows reprogramming if accidently blowing power w/leds
+    delay(2000);
+    FastLED.addLeds<UCS1903, LED_CONTROL_PIN, RGB>(leds, NUM_LEDS);           // <- this part
+}
+
+
+
+void loop() {
+  if( digitalRead( PATTERN_CHANGE_PIN ) )
+  {
+    if ( buttonCount == 2 )
+    {
+      patternId++;
+      patternId %= numPatterns;
+      patternHoldCount = 0;
+    }
+    
+    if ( buttonCount < 10 )
+    {
+      buttonCount++;
+    }
+  }
+  else
+  {
+    buttonCount = 0;
+  }
+
+   PatternFunctions[patternId]();
+   
+   FastLED.show();
+   delay( 10 );
+   patternHoldCount++;
+}
+
+
+
+
+
+
+
+void StripeHorizontal(void)
+{
+    uint16_t delayAfterShow = analogRead( A0 );
+   if ( patternHoldCount < delayAfterShow / 10 )
+   {
+      return;
+   }
+   else
+   {
+      patternHoldCount = 0;
+   }
+   
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) 
+   {
+      if ( whiteLed < NUM_LEDS_IN_7 )
+      {
+        leds[whiteLed] = ColorA;
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 )
+      {
+        leds[whiteLed] = ColorB;
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 )
+      {
+        leds[whiteLed] = ColorA;
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 )
+      {
+        leds[whiteLed] = ColorB;
+      }
+   }
+  {
+    CRGB temp = ColorA;
+    ColorA = ColorB;
+    ColorB = temp;
+  }
+}
+
+
+
+
+void DotChase(void)
+{
+   static uint8_t ledIdx = 0;
+   uint16_t delayAfterShow = analogRead( A0 );
+   if ( patternHoldCount < ( delayAfterShow / 10 ) )
+   {
+      return;
+   }
+   else
+   {
+      patternHoldCount = 0;
+   }
+   
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) 
+   {
+      if ( whiteLed == ledIdx )
+      {
+        leds[whiteLed] = CRGB::White;
+      }
+      else
+      {
+        leds[whiteLed] = CRGB::Black;
+      }
+   }
+
+   ledIdx++;
+   ledIdx %= NUM_LEDS;
+}
+
+void Meter(void)
+{
+   if ( patternHoldCount < 1 )
+   {
+      return;
+   }
+   else
+   {
+      patternHoldCount = 0;
+   }
+   
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) 
+   {
+      if ( float(analogRead( A0 )) / 1024.0 > float(whiteLed) / float(NUM_LEDS) )
+      {
+        leds[whiteLed] = CRGB::White;
+      }
+      else
+      {
+        leds[whiteLed] = CRGB::Black;
+      }
+   }
+}
+
+
+
+
+void Chant(void)
+{
+  static uint8_t chantIdx2 = 0;
+  uint8_t chantIdx = chantIdx2 / 2;
+  uint16_t delayAfterShow = 0;
+   if ( chantIdx2 % 2 == 0 )
+   {
+     if ( chantIdx < 4 )
+     {
+      delayAfterShow = 300;
+     }
+     else
+     {
+      delayAfterShow = 130;
+     }
+   }
+   else
+   {
+      delayAfterShow = 30;
+   }
+
+   if ( patternHoldCount < (delayAfterShow / 10) )
+   {
+      return;
+   }
+   else
+   {
+      patternHoldCount = 0;
+   }
+
+   
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed++) 
+   {
+     if ( chantIdx2 % 2 == 1 )
+     {
+        leds[whiteLed] = CRGB::Black;
+        continue;
+     }
+
+    
+      if ( whiteLed < NUM_LEDS_IN_7 )
+      {
+        if( chantIdx == 3 )
+        {
+          leds[whiteLed] = CRGB::White;
+        }
+        else if ( chantIdx == 4 )
+        {
+          leds[whiteLed] = GoldColor;
+        }
+        else
+        {
+          leds[whiteLed] = CRGB::Black;
+        }
+        
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 )
+      {
+        if( chantIdx == 2 )
+        {
+          leds[whiteLed] = CRGB::White;
+        }
+        else
+        {
+          leds[whiteLed] = CRGB::Black;
+        }
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 )
+      {
+        if( chantIdx == 1 )
+        {
+          leds[whiteLed] = CRGB::White;
+        }
+        else
+        {
+          leds[whiteLed] = CRGB::Black;
+        }
+      }
+      else if ( whiteLed < NUM_LEDS_IN_7 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 + NUM_LEDS_IN_2 )
+      {
+        if( chantIdx == 0 )
+        {
+          leds[whiteLed] = CRGB::White;
+        }
+        else
+        {
+          leds[whiteLed] = CRGB::Black;
+        }
+      }
+   }
+  {
+    CRGB temp = ColorA;
+    ColorA = ColorB;
+    ColorB = temp;
+  }
+
+
+  chantIdx2++;
+  chantIdx2 %= 10;
+}
+
+
+
+
+
+void RainbowFull(void)
+{
+   static uint8_t currentHue = 0;
+   uint16_t delayAfterShow = analogRead( A0 );
+
+   if ( patternHoldCount < (delayAfterShow / 10) )
+   {
+      return;
+   }
+   else
+   {
+      patternHoldCount = 0;
+   }
+   
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) 
+   {
+    leds[whiteLed].setHSV( currentHue, 255, 255 );
+   }
+   
+   currentHue++;
+}
